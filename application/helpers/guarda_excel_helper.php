ad51cbba7b6d5b46b11a03966b1c467f5d79ac97
wbravoanoni <wbravoanoni@gmail.com>
2018-02-24 21:30:30 -0300

    formato hoja cotizaciones

--- application/helpers/guarda_excel_helper.php
+++ application/helpers/guarda_excel_helper.php
@@ -516,22 +516,26 @@ $resultado = $CI->Model_welcome->getCot($idInmo,$idProy,$fInicio,$fFinal);
 
   if($resultado->num_rows() > 0 ){
 
-  //Se agrega la libreria PHPExcel */
     require_once 'lib/PHPExcel/PHPExcel.php';
 
-    // Se crea el objeto PHPExcel
     $objPHPExcel = new PHPExcel();
 
-    // Se asignan las propiedades del libro
     $objPHPExcel->getProperties()->setCreator("TGA") //Autor
                ->setLastModifiedBy("TGA") //Ultimo usuario que lo modificó
-               ->setTitle("Reporte idInmobiliaria")
-               ->setSubject("Reporte Excel con PHP y MySQL")
-               ->setDescription("Reporte idInmobiliaria")
-               ->setKeywords("Reporte idInmobiliaria")
-               ->setCategory("Reporte idInmobiliaria");
+               ->setTitle("Reporte Inmobiliaria")
+               ->setSubject("Reporte Excel Cotizaciones")
+               ->setDescription("Reporte Inmobiliaria")
+               ->setKeywords("Reporte Inmobiliaria")
+               ->setCategory("Reporte Inmobiliaria");
 
-    $tituloReporte = "Reporte Global";
+   
+    $titulogleads   = "G-Leads";
+    $tituloReporte4 = $titulogleads." es parte de la familia de servicios de TGA";
+    $tituloReporte5 = "(562) 2233 4658)";
+    $tituloReporte6 = "Alfredo Barros Errazuriz 1900, OF 204";
+    $tituloReporte7 = "Providencia | Santiago";
+    $tituloReporte8 = "Chile";
+
     $titulosColumnas = array(
 'idCategorizar',
 'Inmobiliaria',
@@ -550,28 +554,30 @@ $resultado = $CI->Model_welcome->getCot($idInmo,$idProy,$fInicio,$fFinal);
 );
     
 $objPHPExcel->setActiveSheetIndex(0)
-->mergeCells('A1:D1');        
-// Se agregan los titulos del reporte
-$objPHPExcel->setActiveSheetIndex(0)
-->setCellValue('A1',  $tituloReporte)
-->setCellValue('A3',  $titulosColumnas[0])
-->setCellValue('B3',  $titulosColumnas[1])
-->setCellValue('C3',  $titulosColumnas[2])
-->setCellValue('D3',  $titulosColumnas[3])
-->setCellValue('E3',  $titulosColumnas[4])
-->setCellValue('F3',  $titulosColumnas[5])
-->setCellValue('G3',  $titulosColumnas[6]) 
-->setCellValue('H3',  $titulosColumnas[7])    
-->setCellValue('I3',  $titulosColumnas[8])  
-->setCellValue('J3',  $titulosColumnas[9])  
-->setCellValue('K3',  $titulosColumnas[10])      
-->setCellValue('L3',  $titulosColumnas[11])    
-->setCellValue('M3',  $titulosColumnas[12])              
-->setCellValue('N3',  $titulosColumnas[13]);
+
+->setCellValue('F4',  $tituloReporte4)
+->setCellValue('F5',  $tituloReporte5)
+->setCellValue('F6',  $tituloReporte6)
+->setCellValue('F7',  $tituloReporte7)
+->setCellValue('F8',  $tituloReporte8)
+->setCellValue('A13',  $titulosColumnas[0])
+->setCellValue('B13',  $titulosColumnas[1])
+->setCellValue('C13',  $titulosColumnas[2])
+->setCellValue('D13',  $titulosColumnas[3])
+->setCellValue('E13',  $titulosColumnas[4])
+->setCellValue('F13',  $titulosColumnas[5])
+->setCellValue('G13',  $titulosColumnas[6]) 
+->setCellValue('H13',  $titulosColumnas[7])    
+->setCellValue('I13',  $titulosColumnas[8])  
+->setCellValue('J13',  $titulosColumnas[9])  
+->setCellValue('K13',  $titulosColumnas[10])      
+->setCellValue('L13',  $titulosColumnas[11])    
+->setCellValue('M13',  $titulosColumnas[12])              
+->setCellValue('N13',  $titulosColumnas[13]);
            
     
     //Se agregan los datos al reporte
-    $i = 4;
+    $i = 14;
 
     foreach ($resultado->result() as $row)
 {
@@ -594,7 +600,8 @@ $objPHPExcel->setActiveSheetIndex(0)
      $i++;   
 }
 $inmobiliaria=$row->Inmobiliaria;
-    $estiloTituloReporte = array(
+
+  $estiloTituloReporte = array(
           'font' => array(
             'name'      => 'Verdana',
               'bold'      => true,
@@ -602,12 +609,12 @@ $inmobiliaria=$row->Inmobiliaria;
                 'strike'    => false,
                 'size'      => 11,
               'color'     => array(
-              'rgb'       => 'FFFFFF'
+              'rgb'       => '000000'
                   )
             ),
           'fill' => array(
         'type'  => PHPExcel_Style_Fill::FILL_SOLID,
-        'color' => array('argb' => 'FF220835')
+       'color' => array('rgb' => 'FFFFFF')
       ),
             'borders' => array(
                 'allborders' => array(
@@ -629,39 +636,100 @@ $inmobiliaria=$row->Inmobiliaria;
                 'color'     => array(
                     'rgb' => '000000'
                 )
+        ),
+          'alignment' =>  array(
+          'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+          'vertical'   => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+          'rotation'   => 0,
+          'wrap'          => TRUE
+          ),
+  'borders' => array(
+    'allborders' => array(
+      'style' => PHPExcel_Style_Border::BORDER_THIN
+    )
+  )
+        );
+
+
+        $estiloTitulogleads = array(
+            'font' => array(
+                'name'      => 'Calibri',
+                'bold'      => true,                          
+                'color'     => array(
+                    'rgb' => '990000'
+                )
         ));
-      
-    $estiloInformacion = new PHPExcel_Style();
-    $estiloInformacion->applyFromArray(
-      array(
-              'font' => array(
-                'name'      => 'Arial',               
+
+          $titulogleadsNegritas = array(
+            'font' => array(
+                'name'      => 'Calibri',
+                'bold'      => true,                          
+                'color'     => array(
+                    'rgb' => '000000'
+                ),
+                'alignment' =>  array(
+                'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                'vertical'   => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+                'rotation'   => 0,
+                'wrap'          => TRUE
+                )
+        ));
+
+          $titulogleads = array(
+            'font' => array(
+                'name'      => 'Calibri',  
+                 'bold'      => false,                                  
+                'color'     => array(
+                    'rgb' => '000000'
+                ),
+                'alignment' =>  array(
+                'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                'vertical'   => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+                'rotation'   => 0,
+                'wrap'          => TRUE
+                )
+        ));
+
+        $todoContenido = array(
+            'font' => array(
+                'name'      => 'Calibri',  
+                 'bold'      => false,                                  
                 'color'     => array(
                     'rgb' => '000000'
                 )
-            ),
-            'fill'  => array(
-        'type'    => PHPExcel_Style_Fill::FILL_SOLID,
-        //'color'   => array('argb' => 'FFd9b7f4')
-      ),
-            'borders' => array(
-                'left'     => array(
-                    'style' => PHPExcel_Style_Border::BORDER_THIN ,
-                  'color' => array(
-                    'rgb' => '3a2a47'
-                    )
-                )             
-            )
+    ),   
+              'borders' => array(
+              'allborders' => array(
+              'style' => PHPExcel_Style_Border::BORDER_THIN
+  )
         ));
-     
+      
+
     $objPHPExcel->getActiveSheet()
-    ->getStyle('A1:D1')->applyFromArray($estiloTituloReporte);
+    ->getStyle('A1:J12')->applyFromArray($estiloTituloReporte);
     $objPHPExcel->getActiveSheet()
-    ->getStyle('A3:AM3')->applyFromArray($estiloTituloColumnas);    
+    ->getStyle('A13:J13')->applyFromArray($estiloTituloColumnas);  
+    $objPHPExcel->getActiveSheet()
+    ->getStyle('F5:F8')->applyFromArray($titulogleads);  
+      $objPHPExcel->getActiveSheet()
+    ->getStyle('F4')->applyFromArray($titulogleadsNegritas);    
+
+     $objPHPExcel->getActiveSheet()
+    ->getStyle('A14:J'.--$i.'')->applyFromArray($todoContenido);   
 
 
+//imagen
+
+$objDrawing = new PHPExcel_Worksheet_Drawing();
+$objDrawing->setName('imgNotice');
+$objDrawing->setDescription('Noticia');
+$img = 'img/G-Leads.png'; // Provide path to your logo file
+$objDrawing->setPath($img);
+$objDrawing->setCoordinates('C3');
+$objDrawing->setHeight(55); // logo height
+$objDrawing->setWorksheet($objPHPExcel->getActiveSheet());
         
-    for($i = 'A'; $i <= 'H'; $i++){
+    for($i = 'A'; $i <= 'N'; $i++){
       $objPHPExcel->setActiveSheetIndex(0)      
         ->getColumnDimension($i)->setAutoSize(TRUE);
     }
@@ -669,9 +737,8 @@ $inmobiliaria=$row->Inmobiliaria;
 
 
 
-    $objPHPExcel->getActiveSheet()->setTitle('Hoja 1');
+    $objPHPExcel->getActiveSheet()->setTitle('Cotizaciones');
     $objPHPExcel->setActiveSheetIndex(0);
-    $objPHPExcel->getActiveSheet(0)->freezePaneByColumnAndRow(0,4);
 
   header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
   header('Content-Disposition: attachment;filename="cotizaciones_'.$inmobiliaria.'_'.$fInicio.'_'.$fFinal.'.xls"');
